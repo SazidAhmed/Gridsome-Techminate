f514160d869f7c10e664c9a75f1bde94b58f3b01
SazidAhmed <sazidahmed.official@gmail.com>
2021-05-09 23:08:59 +0600

    working on filter projects

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -1,14 +1,22 @@
 <template>
     <div>
       <v-container >
-        <p class="pa-5 mt-10 text-h3 font-weight-medium text-uppercase text-center">Completed Projects</p>
+        <p class="pa-5 mt-10 text-h3 font-weight-medium text-uppercase text-center">Made By Us</p>
+        <div class="d-flex justify-center mb-5">
+          <v-btn-toggle v-model="text" tile color="orange accent-3" group>
+            <v-btn @click="filterData(website)" value="left">Design</v-btn>
+            <v-btn @click="filterData(design)" value="center">Website</v-btn>
+            <v-btn @click="filterData(mobile)" value="right">Mobile App</v-btn>
+          </v-btn-toggle>
+        </div>
+
         <v-row>
-          <v-col cols="12" sm="12" md="4" v-for="(service, index) in services" :key="index">
+          <v-col cols="12" sm="12" md="4" lg="3" v-for="(service, index) in services" :key="index">
              <v-card class="mx-auto" max-width="344">
               <v-img :src="service.image" height="250px"></v-img>
               <p class="text-h5 text-center font-weight-bold mt-5">{{ service.title }}</p>
               <div class="d-flex justify-center">
-                <v-btn color="blue white--text mb-5">View Live</v-btn>
+                <v-btn color="orange white--text mb-5">View Live</v-btn>
               </div>
             </v-card>
           </v-col>
@@ -24,17 +32,48 @@ export default {
   },
   data(){
     return{
+      website:'website',
+      design:'design',
+      mobile:'mobile',
+      text: 'center',
+      icon: 'justify',
+      toggle_none: null,
+      toggle_one: 0,
+      toggle_exclusive: 2,
+      toggle_multiple: [0, 1, 2],
       show: false,
       services:[
-        {"title":"Online Store","image":"https://cdn.vuetifyjs.com/images/cards/sunshine.jpg", "color":"blue--text", "number":"01", "description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, quis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, tenetur!"},
-        {"title":"Website Development","icon":"mdi-web", "color":"purple--text", "number":"02", "description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, quis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, tenetur!"},
-        {"title":"UI/UX Design","icon":"mdi-material-design", "color":"red--text", "number":"03", "description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, quis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, tenetur!"},
-        {"title":"Cloud & DevOps","icon":"mdi-cloud-tags", "color":"green--text", "number":"04", "description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, quis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, tenetur!"},
-        {"title":"Analytic Solutions","icon":"mdi-chart-bar-stacked", "color":"orange--text", "number":"05", "description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, quis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, tenetur!"},
-        {"title":"Database Administration","icon":"mdi-microsoft-access", "color":"teal--text", "number":"06", "description":"Lorem, ipsum dolor sit amet consectetur adipisicing elit. Non, quis. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum, tenetur!"}
+        {"title":"Online Store", "type":"website", "image":"https://cdn.vuetifyjs.com/images/cards/sunshine.jpg", "color":"blue--text", "number":"01", },
+        {"title":"Website Development", "type":"design", "icon":"mdi-web", "color":"purple--text", "number":"02",},
+        {"title":"UI/UX Design", "type":"website", "icon":"mdi-material-design", "color":"red--text", "number":"03",},
+        {"title":"Cloud & DevOps", "type":"mobile", "icon":"mdi-cloud-tags", "color":"green--text", "number":"04",},
+        {"title":"Analytic Solutions", "type":"mobile", "icon":"mdi-chart-bar-stacked", "color":"orange--text", "number":"05",},
+        {"title":"Database Administration", "type":"website", "icon":"mdi-microsoft-access", "color":"teal--text", "number":"06",}
       ]
     }
-  }
+  },
+  created(){
+   
+  },
+  watch:{
+    services:{
+       handler: function(val, oldVal) {
+            this.filterData();
+        },
+        deep: true
+    }
+  },
+  methods: {
+   
+  },
+
+  computed:{
+    filterData(type){
+      console.log("filtered", type)
+      return this.services.filter((p)=> p.type.includes(type))
+    }
+  },
+
 }
 </script>
 

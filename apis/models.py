5e34e6ae8ec7a51515a8a102305685ca72051b84
SazidAhmed <sazidahmed.official@gmail.com>
2021-05-16 16:36:52 +0600

    Team Model added

--- apis/models.py
+++ apis/models.py
@@ -27,3 +27,32 @@ class Project(models.Model):
         if self.image:
             return 'http://127.0.0.1:8000' + self.image.url
         return ''
+
+
+
+class Team(models.Model):
+    # Project type
+
+    DESIGN = 'design'
+    WEBSITE = 'website'
+    APP = 'app'
+
+    TEAM_TYPES = (
+        (DESIGN, 'design'),
+        (WEBSITE, 'website'),
+        (APP, 'app')
+    )
+
+    # Fields
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    team_type = models.CharField(max_length=20, choices=TEAM_TYPES, default=WEBSITE)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+        return ''

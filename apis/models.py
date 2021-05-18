0b7a7fb93824fbdf73f31f2238a71a8e6f742528
SazidAhmed <sazidahmed.official@gmail.com>
2021-05-18 12:26:21 +0600

    origin store added

--- apis/models.py
+++ apis/models.py
@@ -61,3 +61,32 @@ class Team(models.Model):
         if self.image:
             return 'http://127.0.0.1:8000' + self.image.url
         return ''
+
+class Store(models.Model):
+    # Project type
+
+    DESIGN = 'design'
+    WEBSITE = 'website'
+    APP = 'app'
+
+    PROJECT_TYPES = (
+        (DESIGN, 'design'),
+        (WEBSITE, 'website'),
+        (APP, 'app')
+    )
+
+    # Fields
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+    project_type = models.CharField(max_length=20, choices=PROJECT_TYPES, default=WEBSITE)
+    image = models.ImageField(upload_to='uploads/', blank=True, null=True)
+
+    def __str__(self):
+        return self.title
+    
+    def get_image(self):
+        if self.image:
+            return 'http://127.0.0.1:8000' + self.image.url
+        return ''
+
